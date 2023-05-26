d3c89ee82a520c53cadf2f7068c30cadb493ba0a
UnsignedArduino <UnsignedArduino@outlook.com>
2023-05-26 16:38:36 -0400

    Add EPaper library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+#include <Adafruit_EPD.h>
+#include <Adafruit_GFX.h>
 #include <Arduino.h>
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <GxEPD2_BW.h>
+#include <GxEPD2_display_selection_new_style.h>
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  display.init(9600, true, 2, false);
+  display.setRotation(0);
+  display.setFont(&FreeMono9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFullWindow();
+  display.fillScreen(GxEPD_WHITE);
+  display.display(false);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-}
+void loop() {}
