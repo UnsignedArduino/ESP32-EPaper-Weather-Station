8f4f896d8828f4adfaac3aefe6c86defaaff9705
UnsignedArduino <UnsignedArduino@outlook.com>
2023-05-30 18:49:54 -0400

    WiFi manager works

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,76 @@
-#include <Adafruit_EPD.h>
 #include <Adafruit_GFX.h>
 #include <Arduino.h>
+#include <Button.h>
 #include <Fonts/FreeMono9pt7b.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include <GxEPD2_BW.h>
 #include <GxEPD2_display_selection_new_style.h>
+#include <WiFi.h>
+#include <WiFiManager.h>
+
+const uint8_t USER_BTN_PIN = 27;
+
+Button userBtn(USER_BTN_PIN);
+
+bool connectToWiFi(bool useScreen) {
+  bool startedConfigAP = false;
+  bool displayedAboutStartedConfigAP = false;
+  bool configAPTimedOut = false;
+  bool displayedAboutConfigAPTimedOut = false;
+
+  WiFiManager wm;
+  WiFi.mode(WIFI_STA);
+  wm.setAPCallback(
+      [&startedConfigAP](WiFiManager* wm) { startedConfigAP = true; });
+  wm.setConfigPortalTimeoutCallback(
+      [&configAPTimedOut]() { configAPTimedOut = true; });
+  wm.setConfigPortalBlocking(false);
+  wm.setConfigPortalTimeout(60);
+  Serial.println("Attempting connection to WiFi");
+
+  if (userBtn.read() == Button::PRESSED) {
+    wm.resetSettings();
+    Serial.println("Removed WiFi configuration");
+  }
+
+  if (!wm.autoConnect("WeatherStationConfig")) {
+    while (true) {
+      if (wm.process()) {
+        break;
+      }
+      if (startedConfigAP && !displayedAboutStartedConfigAP) {
+        Serial.println("Failed to connect to WiFi, starting configuration AP");
+        displayedAboutStartedConfigAP = true;
+      }
+      if (configAPTimedOut && !displayedAboutConfigAPTimedOut) {
+        Serial.println("Configuration AP timed out, exiting");
+        displayedAboutConfigAPTimedOut = true;
+        goto wifiConnectFailed;
+      }
+    }
+  }
+  Serial.println("Successfully connected to saved WiFi network");
+  Serial.print("Connected to: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  Serial.print("Local IPv4 address: ");
+  Serial.println(WiFi.localIP());
+  return true;
+
+wifiConnectFailed:
+  Serial.println("Failed to connect to WiFi");
+  return false;
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+
+  delay(100);
+
+  userBtn.begin();
 
   display.init(9600, true, 2, false);
   display.setRotation(0);
@@ -18,6 +79,8 @@ void setup() {
   display.setFullWindow();
   display.fillScreen(GxEPD_WHITE);
   display.display(false);
+
+  connectToWiFi(true);
 }
 
 void loop() {}
